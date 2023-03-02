4f6bbb44f54404dd773cea74f023e66229a8f2a1
Kevin Chen <kevinchen1688@yahoo.com>
2023-03-02 23:27:32 +0000

    done

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,6 +7,12 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i< arr.length; i++ ){
+            if (arr[i]> max) {
+                max = arr[i];
+            }
+        }
+        return max;
     }
 }
